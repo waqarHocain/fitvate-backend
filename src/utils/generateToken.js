ce9547d73320fa3a285f677e5be5508ac5dfb83d
Waqar Mastan <waqarmaxtan@gmail.com>
2024-03-20 11:06:58 +0500

    Fix JWT expiration

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken");
 const generateToken = (userId) => {
   const token = jwt.sign(
     {
-      expiresIn: "12h",
       id: userId,
     },
-    process.env.JWT_SECRET
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" }
   );
   return token;
 };
