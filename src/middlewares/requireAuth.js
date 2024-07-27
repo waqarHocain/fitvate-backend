49166020fc4d0d04b9c38fcdf0fad5e5ff948f04
Waqar Mastan <waqarmaxtan@gmail.com>
2024-07-28 02:05:26 +0500

    When a user account is deleted, invalidate token

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 
-function requireAuth(req, res, next) {
+const db = require("../services/db");
+
+async function requireAuth(req, res, next) {
   const header = req.headers["authorization"];
   const accessToken = header ? header.split(" ")[1] : null;
   if (!accessToken)
@@ -15,6 +17,21 @@ function requireAuth(req, res, next) {
     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
 
     if (decoded) {
+      // check if the user exists in database
+      const user = await db.user.findUnique({
+        where: {
+          id: decoded.id,
+        },
+      });
+      if (!user) {
+        return res.status(403).json({
+          status: "error",
+          code: 403,
+          timestamp: new Date(),
+          message: "Forbidden.",
+        });
+      }
+
       req.user = { id: decoded.id, email: decoded.email };
       next();
     }
