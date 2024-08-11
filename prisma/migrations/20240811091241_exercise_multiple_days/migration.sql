deffc5be19f0c37eb55a0fc50220f73be424aa8c
Waqar Mastan <waqarmaxtan@gmail.com>
2024-08-11 14:13:26 +0500

    Change schema for Exercise model to be in multiple days

---
+++ prisma/migrations/20240811091241_exercise_multiple_days/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The primary key for the `Exercise` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `Exercise` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX `Exercise_exerciseId_key` ON `Exercise`;
+
+-- AlterTable
+ALTER TABLE `Exercise` DROP PRIMARY KEY,
+    DROP COLUMN `id`,
+    ADD PRIMARY KEY (`exerciseId`, `dayId`, `weekId`, `workoutPlanId`);
